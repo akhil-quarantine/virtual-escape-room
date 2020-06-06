529a8812ab3f0eb50e235818ee4fb3dbf7d3c1a2
Akhil Killawala <brucewratner@gmail.com>
2020-06-06 12:45:35 -0700

    Updates

---
+++ precache-manifest.92a801b7fafc2530e6ec540e2aeed525.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b5a2675bcfc4ce44e24e860bb788bd",
+    "url": "/virtual-escape-room/index.html"
+  },
+  {
+    "revision": "a03897c155f1bdac94c0",
+    "url": "/virtual-escape-room/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f8fed1e7abc313490f26",
+    "url": "/virtual-escape-room/static/css/main.fc2cd764.chunk.css"
+  },
+  {
+    "revision": "a03897c155f1bdac94c0",
+    "url": "/virtual-escape-room/static/js/2.1247c2b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/virtual-escape-room/static/js/2.1247c2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8fed1e7abc313490f26",
+    "url": "/virtual-escape-room/static/js/main.760b280b.chunk.js"
+  },
+  {
+    "revision": "1b536770ed430f772993",
+    "url": "/virtual-escape-room/static/js/runtime-main.8b74a3c2.js"
+  },
+  {
+    "revision": "6a3d686b051fc8faaa43101cc8f6f592",
+    "url": "/virtual-escape-room/static/media/computerback.6a3d686b.png"
+  }
+]);
