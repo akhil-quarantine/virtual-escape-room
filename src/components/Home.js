2ce60463b4f78db73361d2f219d646f9f78fa1d2
Akhil Killawala <brucewratner@gmail.com>
2020-06-07 08:49:35 -0700

    chore(fix);add

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ function Home(props){
                     </p>
                     <p> You can check how much time you have left 
                         in the top right corner and use the right to navigate. More levels will open up 
-                        as you successfully complete tasks, and the puzzles get progressively harder so we recommend
+                        as you successfully complete tasks, and the puzzles get progressively harder so I recommend
                         that you play with other people either by all doing the puzzle separately and sharing findings, or 
                         by screen sharing a single screen using any type of video conferencing tool. A disclaimer I do not take responsibility for any part of the ficton that may 
                         mirror reality, and apologize if anything comes across as offensive as it mostly certainly wasn't 
